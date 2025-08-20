2bd37582199cdda48957737541be3e54264367b8
Mateus Alves <contatomateusalves@hotmail.com>
2025-08-20 19:51:55 -0300

    fix: return json

--- index.ts
+++ index.ts
@@ -2,11 +2,14 @@ import { log } from "./log";
 
 export const handler = async (
   event: any
-): Promise<{ statusCode: number; body: string }> => {
+): Promise<{ statusCode: number; body: string; headers: object }> => {
   log("event:" + JSON.stringify(event));
 
   return {
     statusCode: 200,
     body: JSON.stringify(event),
+    headers: {
+      "Content-Type": "application/json",
+    },
   };
 };
