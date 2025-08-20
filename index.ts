3fa85ad82baf153eedfbe006c3590409aea6f448
Mateus Alves <contatomateusalves@hotmail.com>
2025-08-20 19:37:49 -0300

    add enviroment variables on log

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { log } from "./log";
 export const handler = async (
   event: any
 ): Promise<{ statusCode: number; body: string }> => {
-  log("Log de execução após o GitHub Action. event:" + JSON.stringify(event));
+  log("event:" + JSON.stringify(event));
 
   return {
     statusCode: 200,
--- log.ts
+++ log.ts
@@ -1,3 +1,4 @@
 export function log(message: string): void {
+  console.log("AMBIENTE:", process.env.ENVIRONMENT);
   console.log("Adicionando log via função:", message);
 }
